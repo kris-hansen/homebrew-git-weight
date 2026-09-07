63c4d216aae07e2d0dfc5bfa1cd1c5d7f03794f9
Kris Hansen <kris@4utopia.com>
2026-09-06 22:28:37 -0600

    Use explicit repository option in formula test

--- Formula/git-weight.rb
+++ Formula/git-weight.rb
@@ -36,7 +36,7 @@ class GitWeight < Formula
   test do
     assert_equal "git-weight #{version}\n", shell_output("#{bin}/git-weight --version")
     system "git", "init", "-q", "fixture"
-    report = JSON.parse(shell_output("#{bin}/git-weight summary fixture --json"))
+    report = JSON.parse(shell_output("#{bin}/git-weight summary --repo fixture --json"))
     assert_equal 0, report.fetch("objects").fetch("blob").fetch("count")
   end
 end
